7e559ae8618e20650f801c2bd0f1478ddd5b9efe
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2020-08-19 20:29:26 +0300

    Add project files.

---
+++ HackerRank_FindtheMedian/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the findMedian function below.
+    static int findMedian(int[] arr)
+    {
+        List<int> sortedArr = MergeSort(arr.ToList());
+
+        return sortedArr[arr.Length / 2];
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int result = findMedian(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+
+    private static List<int> MergeSort(List<int> unsorted)
+    {
+        if (unsorted.Count <= 1)
+            return unsorted;
+
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        int middle = unsorted.Count / 2;
+        for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+        {
+            left.Add(unsorted[i]);
+        }
+        for (int i = middle; i < unsorted.Count; i++)
+        {
+            right.Add(unsorted[i]);
+        }
+
+        left = MergeSort(left);
+        right = MergeSort(right);
+        return Merge(left, right);
+    }
+
+    private static List<int> Merge(List<int> left, List<int> right)
+    {
+        List<int> result = new List<int>();
+
+        while (left.Count > 0 || right.Count > 0)
+        {
+            if (left.Count > 0 && right.Count > 0)
+            {
+                if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());      //Rest of the list minus the first element
+                }
+                else
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            else if (left.Count > 0)
+            {
+                result.Add(left.First());
+                left.Remove(left.First());
+            }
+            else if (right.Count > 0)
+            {
+                result.Add(right.First());
+
+                right.Remove(right.First());
+            }
+        }
+        return result;
+    }
+}
+
